884c2e653e1c2eef6255abef36e14f9cc5c88af2
Nelson Sandoval <techednelson@gmail.com>
2018-03-10 22:08:00 +0200

    feat: Add App Shell to cache

--- sw.js
+++ sw.js
@@ -1,6 +1,33 @@
-self.addEventListener('install', e => console.log('[Service Worker], Installing Service Worker ...', e));
+self.addEventListener('install', e => {
+    console.log('[Service Worker], Installing Service Worker ...', e);
+    e.waitUntil(
+        caches.open('static')
+        .then(cache => {
+            console.log('[Service Worker] Precaching App Shell');
+            cache.addAll([
+                '/',
+                '/index.html',
+                '/restaurant.html',
+                '/css/styles.css',
+                '/img/*.jpg',
+                '/js/main.js',
+                '/js/restaurant_info.js',
+                '/js/dbhelper.js'
+            ]);
+        })
+    );
+});
 
 self.addEventListener('activate', e => {
     console.log('[Service Worker], Activating Service Worker ...', e);
     return self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request)
+        .then(response=> {
+            return response || fetch(e.request);
+        })
+    );
 });
